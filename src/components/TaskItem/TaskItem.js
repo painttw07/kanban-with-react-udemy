d3dd92aad94a2d05af9c8b39449faa9a63e94c4f
Pâmela <72814384+painttw07@users.noreply.github.com>
2021-05-16 21:24:18 -0300

    commit 3

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./task-item.css";
 import PropTypes from "prop-types";
 
 export default function TaskItem({ id, title, taskState }) {
-  return <div>{title}</div>;
-}
+  //chave pra controlar o estado da tarefa e quando está editando a tarefa
+  //começa como false
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
 
+  const onTitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle(newTitle);
+  };
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+    }
+  };
+
+  //renderização condicional: renderiza um pedaço do componente dependendo do estado dele
+  if (isEditing) {
+    return (
+      <input
+        type="text"
+        value={editableTitle}
+        onChange={onTitleChange}
+        onKeyPress={onKeyPress}
+      />
+    );
+  } else {
+    return <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>;
+  }
+}
 TaskItem.protoTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
